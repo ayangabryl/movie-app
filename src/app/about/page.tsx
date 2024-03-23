ac3579b9042a132d1fdc9f234a8b1cadca146d73
ayangabryl <iangabrielagujitas@gmail.com>
2024-03-24 01:16:34 +0800

    initialize styles

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import Layout from "../layout";
+
+
+const About = () => {
+    return (
+        <Layout>
+            <h1>About Us</h1>
+            <p>This is the about page of our movie application.</p>
+        </Layout>
+    );
+};
+
+export default About;
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import Layout from "../layout";
+
+const Contact = () => {
+    return (
+        <Layout>
+            <h1>Contact Us</h1>
+            <p>Here's how you can get in touch with us.</p>
+        </Layout>
+    );
+};
+
+export default Contact;
